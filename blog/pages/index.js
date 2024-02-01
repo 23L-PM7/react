a3cad33573d9898cffb8884de2dd1b031851c6dc
erdenetsogta <a.erdenetsogt@gmail.com>
2024-02-01 18:51:49 +0800

    load next pagination

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -4,24 +4,40 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [articles, setArticles] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    fetch("https://dev.to/api/articles?username=j471n")
+    fetch("https://dev.to/api/articles?username=j471n&per_page=9&page=1&tag=discuss")
       .then((response) => response.json())
       .then((data) => {
         setArticles(data);
       });
   }, []);
 
+  function loadNext() {
+    fetch(`https://dev.to/api/articles?username=j471n&per_page=9&page=${currentPage + 1}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setArticles([...articles, ...data]);
+        setCurrentPage(currentPage + 1);
+      });
+  }
+
   if (articles === undefined) return <Loader />;
 
   return (
     <>
       <div className="container mx-auto">
-        <div className="grid grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 gap-5 px-4 md:grid-cols-2 lg:grid-cols-3">
           {articles.map((article) => (
-            <Card key={article.id} article={article} />
+            <Card key={article.id} article={article} profileShown />
           ))}
+        </div>
+
+        <div className="py-8 text-center">
+          <button className="p-6 rounded bg-blue-50 hover:bg-blue-200" onClick={loadNext}>
+            Load more
+          </button>
         </div>
       </div>
     </>
